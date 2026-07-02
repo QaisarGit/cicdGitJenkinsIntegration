8717edc625138007edaf8b1db32b51cfffe4b526
QaisarGit <qaisar.fareed@gmail.com>
2026-07-02 12:59:52 +0530

    git ci cd pipeline integretion

---
+++ cicdGitJenkinsIntegrationClass/src/main/java/cicdGitJenkinsIntegrationPack/cicdGitJenkinsIntegrationClass/App.java
@@ -0,0 +1,10 @@
+package cicdGitJenkinsIntegrationPack.cicdGitJenkinsIntegrationClass;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ cicdGitJenkinsIntegrationClass/src/test/java/CiCdGitJenkinsIntegrationPackage/CICDGitJenkinsIntegrationClass.java
@@ -0,0 +1,12 @@
+package CiCdGitJenkinsIntegrationPackage;
+
+import org.testng.annotations.Test;
+
+public class CICDGitJenkinsIntegrationClass {
+
+	@Test
+	public void SimpleMethod() {
+		
+		System.out.println( " simpleification of cicd git jenkins pipeline integration");
+	}
+}
